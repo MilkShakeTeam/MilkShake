2dcadea3c721fe7f66e701ce78fd158864cb6723
PNSalocin <nicolas.merelli@gmail.com>
2014-07-20 19:38:53 +0200

    Décorateur de gestion des appels ajax avec connexion requise de l'utilisateur

--- Core/decorators.py
+++ Core/decorators.py
@@ -1,15 +1,9 @@
-from django.http import HttpResponseBadRequest
+from django.http import HttpResponse, HttpResponseBadRequest
 
 
 def ajax_required(f):
     """
     AJAX request required decorator
-    use it in your views:
-
-    @ajax_required
-    def my_view(request):
-        ....
-
     """
 
     def wrap(request, *args, **kwargs):
@@ -21,3 +15,24 @@ def ajax_required(f):
     wrap.__name__ = f.__name__
 
     return wrap
+
+
+def login_required_ajax(function=None):
+    """
+    Just make sure the user is authenticated to access a certain ajax view
+
+    Otherwise return a HttpResponse 401 - authentication required
+    instead of the 302 redirect of the original Django decorator
+    """
+    def _decorator(view_func):
+        def _wrapped_view(request, *args, **kwargs):
+            if request.user.is_authenticated():
+                return view_func(request, *args, **kwargs)
+            else:
+                return HttpResponse(status=401)
+        return _wrapped_view
+
+    if function is None:
+        return _decorator
+    else:
+        return _decorator(function)
